56332bb8c34ecf2518444bce02e8ab63b8b0773b
JessGiannini <jessgiannini@gmail.com>
2021-06-28 09:09:23 -0700

    organizing code outline

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,17 @@
 // Title page with button to start quiz
 
 var startPage = (document.getElementById("startPage").style.display = "block");
+var startTime = 5;
+var timerPenalty = 1;
 
 // Onclick of start button everything resets to display the first question and start the timer
+
+startButton.addEventListener("click", function () {
+  //initializeQuiz();
+});
+
+//write inititalizeQuiz function
+
 // User answers the question which displays correct or wrong
 // If wrong the timer deducts 1 minute
 // If correct the score is logged and the next question is displayed
